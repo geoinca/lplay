e25d6ae0a0dac1fc51b7ffa211b8b48f8467ef62
@geo_inca <geo.inca@hotmail.com>
2020-07-01 11:57:41 +0200

    [Ticket: 0001.a]
    	modified:   docker-compose/mysql/init_db.sql
    	add photo table

--- docker-compose/mysql/init_db.sql
+++ docker-compose/mysql/init_db.sql
@@ -8,3 +8,18 @@ CREATE TABLE `places` (
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `places` (name, visited) VALUES ('Berlin',0),('Budapest',0),('Cincinnati',1),('Denver',0),('Helsinki',0),('Lisbon',0),('Moscow',1),('Nairobi',0),('Oslo',1),('Rio',0),('Tokyo',0);
+
+DROP TABLE IF EXISTS `photos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `photos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `place_id` bigint(20) unsigned NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `photos_place_id_foreign` (`place_id`),
+  CONSTRAINT `photos_place_id_foreign` FOREIGN KEY (`place_id`) REFERENCES `places` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
